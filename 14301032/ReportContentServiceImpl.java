be35cbcebd850103473088caafef6d9dfb375c7f
Seven235 <745433907@qq.com>
2016-12-20 12:08:09 +0800

    Create ReportContentServiceImpl.java

---
+++ 14301032/ReportContentServiceImpl.java
@@ -0,0 +1,13 @@
+package cn.edu.bjtu.weibo.service.impl;
+
+import cn.edu.bjtu.weibo.service.ReportContentService;
+
+public class ReportContentServiceImpl implements ReportContentService{
+	public boolean reportWeibo(String userId, String weiboId) {
+		return true;
+	}
+
+	public boolean reportComment(String userId, String commentId) {
+		return true;
+	}
+}
